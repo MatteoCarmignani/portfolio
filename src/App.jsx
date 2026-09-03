a15d5c62d0fab81c277ccba0ced38ead3b9551c0
MatteoCarmignani <matteo.carmignani01@gmail.com>
2026-09-03 15:02:20 +0200

    add initial navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Navbar from './components/Navbar'
+
 function App() {
   return (
-    <div>
-      <h1>Ciao Matteo</h1>
-      <p>Portfolio React + Vite</p>
-    </div>
+    <>
+      <Navbar />
+    </>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>Matteo</h2>
+
+      <ul className="navbar-menu">
+        <li>Home</li>
+        <li>Chi sono</li>
+        <li>Competenze</li>
+        <li>Progetti</li>
+        <li>Contatti</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
