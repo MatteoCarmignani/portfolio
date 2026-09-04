4bfd4b305b8636e000af36bff527a50e74eafde0
MatteoCarmignani <matteo.carmignani01@gmail.com>
2026-09-04 16:35:56 +0200

    add initial hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import './Hero.css'
+
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <p>Ciao, sono</p>
+
+        <h1>Matteo Carmignani</h1>
+
+        <h2>Junior Web Developer</h2>
+
+        <p>
+          Creo applicazioni web moderne utilizzando React e JavaScript.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
