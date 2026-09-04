4cda8b3c4c6f8a81bb8877fc462d19c0723d97dd
MatteoCarmignani <matteo.carmignani01@gmail.com>
2026-09-04 19:57:39 +0200

    add Skill page

---
+++ src/pages/Skills.jsx
@@ -0,0 +1,37 @@
+import './Skills.css'
+
+function Skills() {
+  return (
+    <section className="skills">
+      <div className="skills-content">
+        <p className="section-subtitle">Le mie competenze</p>
+
+        <h1>Skills</h1>
+
+        <div className="skills-list">
+          <div className="skill-card">
+            <h2>HTML</h2>
+            <p>Attualmente nulla</p>
+          </div>
+
+          <div className="skill-card">
+            <h2>CSS</h2>
+            <p>Sempre nulla</p>
+          </div>
+
+          <div className="skill-card">
+            <h2>JavaScript</h2>
+            <p>Ovviamente nulla</p>
+          </div>
+
+          <div className="skill-card">
+            <h2>React</h2>
+            <p>0 anche di questo</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Skills
