dcacb43c7120e990e56fecc1d0a975178763e48f
MatteoCarmignani <matteo.carmignani01@gmail.com>
2026-09-11 15:39:44 +0200

    add carousel in projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import ProjectCard from '../components/ProjectCard'
 import './Projects.css'
 
@@ -38,6 +39,31 @@ function Projects() {
     }
   ]
 
+  const [currentPage, setCurrentPage] = useState(0)
+
+  const projectsPerPage = 2
+
+  const startIndex = currentPage * projectsPerPage
+
+  const visibleProjects = projects.slice(
+    startIndex,
+    startIndex + projectsPerPage
+  )
+
+  const totalPages = Math.ceil(projects.length / projectsPerPage)
+
+  function nextPage() {
+    if (currentPage < totalPages - 1) {
+      setCurrentPage(currentPage + 1)
+    }
+  }
+
+  function previousPage() {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1)
+    }
+  }
+
   return (
     <main className="projects">
 
@@ -49,22 +75,46 @@ function Projects() {
 
         <h1>I miei progetti</h1>
 
-        <div className="projects-list">
+        <div className="projects-carousel">
 
-          {projects.map((project) => (
-            <ProjectCard
-              key={project.title}
-              title={project.title}
-              slug={project.slug}
-              description={project.description}
-              image={project.image}
-              technologies={project.technologies}
-              github={project.github}
-              demo={project.demo}
-            />
-          ))}
+          <button
+            className="carousel-button"
+            onClick={previousPage}
+            disabled={currentPage === 0}
+          >
+            ←
+          </button>
+
+          <div className="projects-list">
+
+            {visibleProjects.map((project) => (
+              <ProjectCard
+                key={project.title}
+                title={project.title}
+                slug={project.slug}
+                description={project.description}
+                image={project.image}
+                technologies={project.technologies}
+                github={project.github}
+                demo={project.demo}
+              />
+            ))}
+
+          </div>
+
+          <button
+            className="carousel-button"
+            onClick={nextPage}
+            disabled={currentPage === totalPages - 1}
+          >
+            →
+          </button>
 
         </div>
+
+        <p className="carousel-counter">
+          {currentPage + 1} / {totalPages}
+        </p>
 
       </div>
 
